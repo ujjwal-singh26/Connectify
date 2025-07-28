45b1e000d61ab0bd4d896948b7cefe5880fbcfa4
ujjwal-singh26 <singhujjwal1703@gmail.com>
2025-07-28 21:38:09 +0530

    Deleted all files

--- frontend/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from "@reduxjs/toolkit"
-import userSlice from "./userSlice"
-import messageSlice from "./messageSlice"
-export const store=configureStore({
-    reducer:{
-        user:userSlice,
-        message:messageSlice
-    }
-})
